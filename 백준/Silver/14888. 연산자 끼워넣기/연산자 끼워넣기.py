14113725611ff7ac0730cd0a3bd893aade2c5c13
Taeseok LEE <106011008+TaeSeokL@users.noreply.github.com>
2024-01-23 18:11:45 +0900

    Update 연산자 끼워넣기.py

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,4 +1,5 @@
 def dfs(L,cnt):
+    # 종료조건일때 최대와 최소를 갱신 후 종료
     if L == n-1:
         global max_val
         global min_val
@@ -6,6 +7,9 @@ def dfs(L,cnt):
         min_val = min(min_val,cnt)
         return
     else:
+        # 연산자 리스트 길이만큼 반복문
+        # 할당된 연산자 수가 0이 아닐때만 사용가능함
+        # 1개 사용했으니 -1 해주고 연산자에 맞춰 연산실행
         for i in range(4):
             if sign[i] != 0:
                 sign[i] -= 1
@@ -16,11 +20,15 @@ def dfs(L,cnt):
                 elif i == 2:    # 곱셈
                     res = cnt * arr[L + 1]
                 else:           # 나눗셈
+                    # 음수 나눗셈의 경우 문제의 조건에서 양수로 변환 후
+                    # 나누고 다시 음수로 변환하라는 조건이 있어 조건 추가
                     if cnt<0:
                         res = - (abs(cnt)//arr[L+1])
                     else:
                         res = cnt // arr[L + 1]
 
+                # 연산결과와 다음 수열을 위한 인덱스를 전달
+                # 그리고 재귀가 종료될때는 꼭 사용한 연산자를 다시 추가시켜주는게 중요하다.
                 dfs(L + 1, res)
                 sign[i] += 1
 
@@ -31,7 +39,7 @@ if __name__ == '__main__':
     max_val = -10000000000                  # 최대값
     min_val = 10000000000                   # 최소값
 
-    dfs(0,arr[0])
+    dfs(0,arr[0])                        # 백트래킹
 
     print(max_val)
     print(min_val)
