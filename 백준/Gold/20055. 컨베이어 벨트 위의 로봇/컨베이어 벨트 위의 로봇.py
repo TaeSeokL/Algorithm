579c58078aaca4b9ba7c679f5913c3e3e9beebb9
Taeseok LEE <106011008+TaeSeokL@users.noreply.github.com>
2024-02-25 20:31:52 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 416 ms, Memory: 110748 KB -BaekjoonHub

---
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -0,0 +1,43 @@
+if __name__=='__main__':
+    n, k = map(int,input().split())         # n 벨트수, k 조건
+    belt = list(map(int,input().split()))   # 벨트정보
+    robot = [0]*n                           # 로봇위치
+    ans = 1                                 # 단계
+
+    while True:
+        # 1번 과정 : 벨트 회전, 로봇 위치 하나씩 증가
+        belt.insert(0,belt.pop(-1))
+        robot.pop(-1)
+        robot.insert(0,0)
+
+        # 2번과정
+        # 로봇배열의 마지막부터 시작해서 끝에 로봇이 있으면 내리기
+        # 나머지칸과 해당칸 +1 칸을 검사하며 해당칸에 로봇이 있고, 다음칸에 로봇이 없고
+        # 내구도가 1이상일때 로봇 이동시켜주기
+        for i in range(n-1,-1,-1):
+            if i == n-1 and robot[i] == 1:
+                robot[i] = 0
+            elif robot[i] == 1 and robot[i+1] == 0 and belt[i+1]>=1:
+                belt[i+1] -= 1
+                robot[i] = 0
+                robot[i+1] = 1
+
+        # 3번과정
+        # 올리는 위치에 로봇이 없다면 로봇 올려주고 내구도 감소
+        if belt[0] != 0:
+            robot[0] = 1
+            belt[0] -= 1
+
+        # 4번과정
+        # 내구도 0인 칸 세어주고, k 이상이면 코드 종료
+        # 아니면 계속 진행
+        res = 0
+        for x in belt:
+            if x == 0:
+                res += 1
+
+        if res >= k:
+            print(ans)
+            break
+        else:
+            ans += 1
