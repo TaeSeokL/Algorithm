8d6560f9a902d31ae40b0dc7f2a858f926f1c796
Taeseok LEE <106011008+TaeSeokL@users.noreply.github.com>
2024-02-27 19:05:36 +0900

    Update 마법사 상어와 파이어볼.py

--- 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.py
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.py
@@ -1,8 +1,9 @@
-
+# 공을 이동하는 함수
+# 완전탐색을 통해 해결했음.
 def move_ball():
     for y in range(n):
         for x in range(n):
-            # 칸에 공이 하나 있을때
+            # 칸에 공이 있을때 공 갯수만큼 반복문으로 처리해줌.
             if len(board[y][x]) != 0:
                 ln = len(board[y][x])   # 공갯수
                 for i in range(ln):
@@ -10,8 +11,11 @@ def move_ball():
                     m,s,d = board[y][x][i]
                     real_move_block = s%n
                     ny,nx = y,x
+
+                    # 실제 움직여야할 블럭수만큼의 반복문
                     for _ in range(real_move_block):
                         # 행과 열의 기존 위치에 따라 경우의 수를 나누고 따로 계산해줌.
+                        # 모든 블럭이 연결되어있으므로 이렇게 처리했음.
                         # 행부터
                         if ny == 0 and (d==0 or d == 1 or d == 7):
                             ny = n-1
@@ -27,17 +31,19 @@ def move_ball():
                             nx = 0
                         else:
                             nx = nx + dir[d][1]
-
+                    # 예비배열에 저장해놓음
                     next_board[ny][nx] += [(m,s,d)]
+                # 원본 배열 값 초기화 해줌.
                 board[y][x] = []
 
+    # 예비배열에 들어잇는걸 원본 배열로 옮겨줌
     for y in range(n):
         for x in range(n):
             if len(next_board[y][x]) != 0:
                 board[y][x] = next_board[y][x]
                 next_board[y][x] = []
 
-
+# 공을 뿌리는 함수
 def spread_ball():
     for y in range(n):
         for x in range(n):
@@ -51,13 +57,14 @@ def spread_ball():
                 flag1 = False   # 모두 짝수인지 판단
                 flag2 = False   # 모두 홀수인지 판단
 
+                # 질량합, 속력합, 방향 배열 얻기
                 for i in range(ln):
                     m,s,d = board[y][x][i]
                     sum_m += m
                     sum_s += s
                     dir_judge[i] = d
 
-                # 각 공의 질량과 속력을 구해줌.
+                # 새로운 공의 질량과 속력을 구해줌.
                 each_ball_m = sum_m//5
                 each_ball_s = sum_s // ln
 
@@ -79,6 +86,7 @@ def spread_ball():
                 else:
                     flag2 = True
 
+                # 모두 짝수이거나 홀수일때 / 아닐때
                 if (flag1 or flag2):
                     for d in range(0,7,2):
                         next_board[y][x] += [(each_ball_m,each_ball_s,d)]
@@ -86,27 +94,26 @@ def spread_ball():
                     for d in range(1,8,2):
                         next_board[y][x] += [(each_ball_m,each_ball_s,d)]
 
+    # 흩뿌려준 공을 원본배열에 옮겨줌.
     for y in range(n):
         for x in range(n):
             if len(next_board[y][x]) != 0:
                 board[y][x] = next_board[y][x]
                 next_board[y][x] = []
 
-
-
-
 if __name__=='__main__':
-    n, m, k = map(int,input().split())                      # n 맵크기, m 공 갯수, k 명령갯수
+    n, m, k = map(int,input().split())                              # n 맵크기, m 공 갯수, k 명령갯수
 
-    board = [[[] for _ in range(n)] for _ in range(n)]      # 맵 (3차원배열)
-    next_board = [[[] for _ in range(n)] for _ in range(n)] # 계산용 맵
+    board = [[[] for _ in range(n)] for _ in range(n)]              # 맵 (3차원배열)
+    next_board = [[[] for _ in range(n)] for _ in range(n)]         # 계산용 맵
     dir = [(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)]   # 방향
-    ball = [0]*(m+1)
 
+    # 맵에 공 표시
     for i in range(1,m+1):
         r,c,m,s,d = map(int,input().split())            # 위치 : (r,c) m 질량, s 속력, d 방향
         board[r-1][c-1] = [(m,s,d)]                     # 각 공의 질량 속력 방향만 저장
 
+    # 명령 갯수만큼 진행
     for _ in range(k):
         # 공 이동
         move_ball()
@@ -114,6 +121,7 @@ if __name__=='__main__':
         # 공 분해
         spread_ball()
 
+    # 남은 공 질량 총합 구하기
     ans = 0
     for y in range(n):
         for x in range(n):
