c74fc7b5f4f2e986097add62d8d309c5b1e92a99
Taeseok LEE <106011008+TaeSeokL@users.noreply.github.com>
2024-03-24 18:47:50 +0900

    [Gold I] Title: 낚시왕, Time: 428 ms, Memory: 147448 KB -BaekjoonHub

---
+++ 백준/Gold/17143. 낚시왕/낚시왕.py
@@ -0,0 +1,63 @@
+if __name__=='__main__':
+    R, C, m = map(int,input().split())      # (r,c) 격자 크기, m 상어수
+
+    # 상어 군집 정보 저장
+    shark = []
+    for _ in range(m):
+        a,b,s,d,z = map(int,input().split())
+        shark.append([a-1,b-1,s,d-1,z])
+
+    dir = [(-1,0),(1,0),(0,1),(0,-1)]   # 상어 이동 방향 상 하 우 좌
+    ans = 0                             # 낚시왕이 잡은 상어 크기 합
+    left_right_idx = [i for i in range(C)] + [i for i in range(C-2,0,-1)]   # 좌우방향상어 위치업데이트 배열
+    up_down_idx = [i for i in range(R)] + [i for i in range(R-2,0,-1)]      # 위아래방향상어 위치업데이트 배열
+
+    # 낚시왕 시작
+    for j in range(C):
+        # [1] 가장 가까운 상어 찾기 -> 행기준으로 정렬
+        shark.sort(key=lambda x:x[0])
+        # 정렬 후 상어왕과 열이 같은 상어를 찾으면 그 상어가 가장 가까이 있는 상어이다. 바로 제거 후 ans 갱신
+        for sh in range(len(shark)):
+            r,c,s,d,z = shark[sh]
+            if c == j:
+                shark.pop(sh)
+                ans += z
+                break
+
+        # [2] 상어 이동
+        for sh in range(len(shark)):
+            y,x,s,d,z = shark[sh]
+
+            if d == 2 or d == 3:     # 좌우 방향 상어 이동
+                x = (x+dir[d][1]*s)%(2*C-2)
+
+                if x >= C:       # C보다 크면 방향 반대
+                    x = left_right_idx[x]
+                    if d == 2 :
+                        d = 3
+                    else:
+                        d = 2
+
+            elif d == 0 or d == 1:
+                y = (y+dir[d][0]*s)%(2*R-2)
+
+                if y >= R:
+                    y = up_down_idx[y]
+
+                    if d == 0:
+                        d = 1
+                    else:
+                        d = 0
+
+            # 위치 및 방향 업데이트
+            shark[sh] = [y,x,s,d,z]
+
+        # [3] 위치가 겹치는 상어 잡아먹기 -> 행과 열 기준으로 오름차순 정렬, 크기 기준으로 내림차순 정렬
+        shark.sort(key=lambda x:(x[0],x[1],-x[4]))
+
+        # 상어 배열을 거꾸로 돌아주면서 그 전꺼랑 위치가 겹치면 현재꺼를 pop 해주기
+        for i in range(len(shark)-1,0,-1):
+            if (shark[i][0],shark[i][1]) == (shark[i-1][0],shark[i-1][1]) :
+                shark.pop(i)
+
+    print(ans)
